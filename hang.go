acb550e34d67f60ae2426b4a72631f6a0e0d8b3b
baurelie <aurelien.branco@ynov.com>
2023-12-19 16:09:17 +0100

    remove txt files

--- hang.go
+++ hang.go
@@ -2,7 +2,6 @@ package hangman
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"math/rand"
 	"os"
@@ -16,31 +15,27 @@ type Game struct {
 	LettresRevelees map[int]bool
 	Tentatives      int
 	Positions       []int
+	MotAffiche      string
 }
 
-func (g *Game) Restore() error {
-	file, err := os.Open("save.txt")
-	if err != nil {
-		return fmt.Errorf("Error opening save file: %v", err)
+func (g *Game) Display() {
+	motAffiche := ""
+	for i, lettre := range g.MotAleatoire {
+		if g.LettresRevelees[i] {
+			motAffiche += strings.ToUpper(string(lettre)) + " "
+		} else {
+			motAffiche += "_ "
+		}
 	}
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(g); err != nil {
-		return fmt.Errorf("Error decoding save file: %v", err)
-	}
-
-	fmt.Println("Game restored successfully.")
-	return nil
+	g.MotAffiche = motAffiche
 }
 
-func NewGame() *Game {
+func NewGame(g *Game) {
 	rand.Seed(time.Now().Unix())
 
 	var words []string
 	var fileName string
 
-	println()
 	fileName = "words.txt"
 
 	file, err := os.Open(fileName)
@@ -62,58 +57,14 @@ func NewGame() *Game {
 		lettresRevelees[randIndex] = true
 	}
 
-	return &Game{
-		Words:           words,
-		MotAleatoire:    motAleatoire,
-		LettresRevelees: lettresRevelees,
-		Tentatives:      10,
-	}
-}
-
-func (g *Game) Save() {
-	save, err := json.Marshal(g)
-	if err != nil {
-		fmt.Println("Error marshaling game:", err)
-		os.Exit(1)
-	}
-
-	file, err := os.Create("save.txt")
-	if err != nil {
-		fmt.Println("Error creating save file:", err)
-		os.Exit(2)
-	}
-	defer file.Close()
-
-	_, err = file.Write(save)
-	if err != nil {
-		fmt.Println("Error writing to save file:", err)
-		os.Exit(3)
-	}
-}
-
-func (g *Game) Display() {
-	reset := "\033[0m"
-	gray := "\033[30m"
-
-	motAffiche := ""
-	for i, lettre := range g.MotAleatoire {
-		if g.LettresRevelees[i] {
-			motAffiche += strings.ToUpper(string(lettre)) + " "
-		} else {
-			motAffiche += "_ "
-		}
-	}
-	fmt.Println(gray + "---------------------------------" + reset)
-	fmt.Printf("%s\n", motAffiche)
-	fmt.Println(gray + "---------------------------------" + reset)
+	g.Words = words
+	g.MotAleatoire = motAleatoire
+	g.LettresRevelees = lettresRevelees
+	g.Tentatives = 10
 }
 
 func (g *Game) Play() {
 	g.Positions = []int{72, 64, 56, 48, 40, 32, 24, 16, 8, 0}
-	green := "\033[32m"
-	red := "\033[31m"
-	gray := "\033[30m"
-	reset := "\033[0m"
 
 	var lettresSuggerees []string
 
@@ -126,7 +77,6 @@ func (g *Game) Play() {
 
 		if input == "STOP" {
 			fmt.Println("To resume the game you must --startWith save.txt")
-			g.Save()
 			return
 		}
 
@@ -151,8 +101,6 @@ func (g *Game) Play() {
 			if !lettreTrouvée {
 				g.Tentatives--
 				fmt.Printf("Not present in the word, %d attempts remaining\n", g.Tentatives)
-				Positions := Positions(g)
-				fmt.Println(Positions)
 			}
 		} else if len(input) >= 2 {
 			mot := strings.ToUpper(input)
@@ -163,16 +111,10 @@ func (g *Game) Play() {
 					g.LettresRevelees[i] = true
 				}
 				g.Display()
-				fmt.Printf(green + "Congrats !\n")
-				fmt.Println(gray + "---------------------------------" + reset)
-				fmt.Println("          END OF GAME")
-				fmt.Println(gray + "---------------------------------" + reset)
 				return
 			} else {
 				g.Tentatives -= 2
 				fmt.Printf("Incorrect word, %d attempts remaining\n", g.Tentatives)
-				Positions := Positions(g)
-				fmt.Println(Positions)
 			}
 		} else {
 			fmt.Println("Invalid input. Please enter a single letter or a word of at least two characters.")
@@ -187,18 +129,9 @@ func (g *Game) Play() {
 		}
 		if motDevine {
 			g.Display() // Afficher le mot à deviner
-			fmt.Printf(green + "Congrats !\n")
-			fmt.Println(gray + "---------------------------------" + reset)
-			fmt.Println("          END OF GAME")
-			fmt.Println(gray + "---------------------------------" + reset)
-			return // Quitter la fonction
+			return      // Quitter la fonction
 		}
 	}
-	fmt.Println(gray + "---------------------------------" + reset)
-	fmt.Printf(red+"Sorry, you didn't guess the word. The word was '%s'.\n", g.MotAleatoire)
-	fmt.Println(gray + "---------------------------------" + reset)
-	fmt.Println("          END OF GAME")
-	fmt.Println(gray + "---------------------------------" + reset)
 }
 
 func isLetter(s string) bool {
@@ -212,29 +145,4 @@ func contains(liste []string, lettre string) bool {
 		}
 	}
 	return false
-}
-
-func Positions(g *Game) string {
-	hangmandraw := ""
-	file, err := os.Open("hangman.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	line := 0
-	if g.Tentatives < len(g.Positions) {
-		for scanner.Scan() {
-			line++
-			if line > g.Positions[g.Tentatives] && line <= g.Positions[g.Tentatives]+7 {
-				hangmandraw += "\n" + scanner.Text()
-			}
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-	}
-	return hangmandraw
 }
